460d8e15eb5a08bc91e0272c18b435226aec8b5c
jackcheng1021 <jackcheng1021@126.com>
2022-11-04 18:06:19 +0800

    a

---
+++ liberty-expect-test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source liberty-openrc.sh
+
+/usr/bin/expect << FLAGEOF
+set timeout 2
+spawn ssh $controller_user@$controller_ip   
+expect {
+        "(yes/no)" {send "yes\r"; exp_continue}
+        "password:" {send "$controller_user_pass\r"}
+}
+expect "${controller_user}@*"  {send "echo \"hello test\" &> ~/test.txt\r"}
+expect "${controller_user}@*"  {send "exit\r"}
+expect eof 
+FLAGEOF
